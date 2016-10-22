098bb205d259e1092bb51d6c39ee4e32fe43ee37
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-10-22 17:57:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$1" =~ ^docker.* ]; then
     if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
         : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
-        sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+        sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /etc/kibana/kibana.yml
     else
         "No ES URL parameter, starting local instance... "
         service elasticsearch start
@@ -16,10 +16,10 @@ else
         sleep 5
 fi
 
-# Patch demo kibi to use standard ES port
-perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibana/config/kibana.yml
-perl -p -i -e "s/\#server.host/server.host/" /opt/kibana/config/kibana.yml
+# Patch configuration for Docker container
+perl -p -i -e "s/localhost/0.0.0.0/" /etc/kibana/kibana.yml
+perl -p -i -e "s/\#server.host/server.host/" /etc/kibana/kibana.yml
 
-# Start Kibi
-/opt/kibana/bin/kibana &
+# Start Kibana 5
+service kibana start &
 tail -f /var/log/elasticsearch/*.log
